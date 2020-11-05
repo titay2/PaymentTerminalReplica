846d6a9319e2c1a15c272e8b2a454da0034d06ec
Tehetena Mashresha <tehetena.mashresha@semel.fi>
2020-11-05 12:50:41 +0200

    Edit and add new forms for the Merchant Detail

--- ClientApp/src/app/merchat-detail/detail/detail.component.ts
+++ ClientApp/src/app/merchat-detail/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-detail',
@@ -8,40 +9,67 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class DetailComponent implements OnInit {
   id = localStorage.getItem("selectedMerchant");
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  editModeLocation = false;
+  editMode = false;
+  locationForm: FormGroup;
+  settlmentForm: FormGroup;
+  options = ['Immidietly', 'Later'];
+  editorTitleLocation = '';
+  editorTitleContract = ''
 
-  ngOnInit(): void {
-    if (!this.id) {
-      this.router.navigate(['/Merchant']);
-    }
+  merchants = {
+    companyName: "Semel OY",
+    businessId: "124534",
+    phoneNo: "+350443001166",
+    StreetAddress: "Valimotie 21",
+    Post: "00540",
+    city: "Helsinki",
+    country: "Finland",
+    contactPresom: "Timo",
+    cpemail: "timo@semel.fi",
+    salesContract: [
+      {
+        type: "Elavon EUR(Visa/MasterCard/Amex/DC)",
+        number: 254786,
+        code: 12345,
+        description: "Automobile parking and Garage"
+      },
+      {
+        type: "Elavon EUR(Visa/MasterCard/Amex/DC)",
+        number: 254786,
+        code: 12345,
+        description: "Automobile parking and Garage"
+      },
+      {
+        type: "Elavon EUR(Visa/MasterCard/Amex/DC)",
+        number: 254786,
+        code: 12345,
+        description: "Automobile parking and Garage"
+      },
+
+    ],
+    terminals: [
+      {
+        type: "SPm20 Semel EUR (FI)",
+        name: "Semel",
+        hardwareId: 1234556,
+        terminalID: 18357,
+        orderTerm: new Date(),
+      }, {
+        type: "SPm20 Semel EUR (FI)",
+        name: "Semel",
+        hardwareId: 1234556,
+        terminalID: 18357,
+        orderTerm: new Date(),
+      }, {
+        type: "SPm20 Semel EUR (FI)",
+        name: "Semel",
+        hardwareId: 1234556,
+        terminalID: 18357,
+        orderTerm: new Date(),
+      }
+    ]
   }
-  merchants = [
-    {
-      companyName: "Semel OY",
-      businessId: "124534",
-      phoneNo: "+350443001166",
-      StreetAddress: "Valimotie 21",
-      Post: "00540",
-      city: "Helsinki",
-      country: "Finland",
-      contactPresom: "Timo",
-      cpemail: "timo@semel.fi"
-    },
-    {
-      companyName: "Semel OY one",
-      businessId: "124445",
-      phoneNo: "+350443001166",
-      StreetAddress: "Valimotie 21",
-      Post: "00540",
-      city: "Helsinki",
-      country: "Finland",
-      contactPresom: "Test",
-      cpemail: "test@semel.fi"
-
-    }
-  ];
-
-
   accordionData = [
     {
       name: "Semel Oy, ATX_PALVELU NETX",
@@ -62,5 +90,92 @@ export class DetailComponent implements OnInit {
         " moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod."
     }
   ];
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+    if (!this.id) {
+      this.router.navigate(['/Merchant']);
+    }
+  }
+  onEditContract(settlment) {
+    this.editMode = true;
+    this.editorTitleContract = 'Edit Settlment Contract'
+    this.settlmentForm = new FormGroup({
+      'contractType': new FormControl(settlment.type, Validators.required),
+      'contractNumber': new FormControl(settlment.number, Validators.required),
+      'merchantCode': new FormControl(settlment.code, Validators.required),
+      'option': new FormControl('', Validators.required),
+      'startTime': new FormControl(settlment.startTime, Validators.required),
+      'endTime': new FormControl(settlment.endTime, Validators.required),
+    })
+  }
+  onNewContract() {
+    this.editMode = false;
+    this.editorTitleContract = 'New Settlment Contract'
+    this.settlmentForm = new FormGroup({
+      'contractType': new FormControl('', Validators.required),
+      'contractNumber': new FormControl('', Validators.required),
+      'merchantCode': new FormControl('', Validators.required),
+      'option': new FormControl('', Validators.required),
+      'startTime': new FormControl('', Validators.required),
+      'endTime': new FormControl('', Validators.required),
+    })
+  }
 
+  onSubmitContract() {
+    const test = {
+      contractType: this.settlmentForm.value['contractType'],
+      contractNumber: this.settlmentForm.value['contractNumber'],
+      merchantCode: this.settlmentForm.value['merchantCode'],
+      option: this.settlmentForm.value['option'],
+      startTime: this.settlmentForm.value['startTime'],
+      endTime: this.settlmentForm.value['endTime'],
+    };
+    if (this.editMode) {
+      console.log('edit' + this.editMode);
+      console.log(test);
+    } else {
+      console.log('edit' + this.editMode);
+      console.log(test);
+    }
+  }
+
+  onEditLocation(m) {
+    this.editModeLocation = true;
+    this.editorTitleLocation = 'Edit Sales Location'
+    this.locationForm = new FormGroup({
+      'locationName': new FormControl(m.companyName, Validators.required),
+      'phoneNumber': new FormControl(m.phoneNo, Validators.required),
+      'post': new FormControl(m.Post, Validators.required),
+      'city': new FormControl(m.city, Validators.required),
+      'country': new FormControl(m.country, Validators.required),
+    })
+  }
+  onAddNewLocation() {
+    this.editModeLocation = false;
+    this.editorTitleLocation = 'New Sales Location'
+    this.locationForm = new FormGroup({
+      'locationName': new FormControl('', Validators.required),
+      'phoneNumber': new FormControl('', Validators.required),
+      'post': new FormControl('', Validators.required),
+      'city': new FormControl('', Validators.required),
+      'country': new FormControl('', Validators.required),
+    })
+  }
+
+  onSubmit() {
+    const test = {
+      locationName: this.locationForm.value['locationName'],
+      phoneNumber: this.locationForm.value['phoneNumber'],
+      post: this.locationForm.value['post'],
+      city: this.locationForm.value['city'],
+      country: this.locationForm.value['country']
+    };
+    if (this.editModeLocation) {
+      console.log('edit' + this.editModeLocation);
+      console.log(test);
+    } else {
+      console.log('edit' + this.editModeLocation);
+      console.log(test);
+    }
+  }
 }
